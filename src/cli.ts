0303329bd9e706d9a347da266e80787b00da69b1
redstonekasi <kasi@hanneskann.de>
2024-02-13 16:27:41 +0100

    split up CLI and API into separate files

---
+++ src/cli.ts
@@ -0,0 +1,51 @@
+import ts from "typescript";
+import * as path from "node:path";
+import * as fs from "node:fs/promises";
+import { parseDefinition } from "./def.js";
+import { createModuleDeclarations } from "./core.js";
+
+function getProjectProgram(filePath: string) {
+	const configPath = ts.findConfigFile(filePath, ts.sys.fileExists, "tsconfig.json");
+	if (!configPath) throw new Error("invalid project, no tsconfig.json was found");
+	const { config } = ts.readConfigFile(configPath, ts.sys.readFile);
+	const { options, fileNames, errors } = ts.parseJsonConfigFileContent(config, ts.sys, path.dirname(configPath));
+	return ts.createProgram({
+		options,
+		rootNames: fileNames,
+		configFileParsingDiagnostics: errors,
+	});
+}
+
+async function main(argv: string[]) {
+	if (argv.length < 1) {
+		console.error("usage: treetype <definition file>");
+	}
+
+	const file = path.resolve(argv[0]);
+	let raw: string;
+	try {
+		raw = await fs.readFile(file, "utf8");
+	} catch (e) {
+		throw new Error(`failed to read definition file ${e instanceof Error ? e.message : e}`);
+	}
+
+	const program = getProjectProgram(file);
+	const definitions = parseDefinition(raw);
+
+	const nodes = createModuleDeclarations(program, definitions, path.dirname(file))
+	const result = ts.createSourceFile("output.ts", "", ts.ScriptTarget.ESNext, false, ts.ScriptKind.TS);
+	const printer = ts.createPrinter({
+		newLine: ts.NewLineKind.LineFeed,
+	});
+	for (const node of nodes) {
+		const str = printer.printNode(ts.EmitHint.Unspecified, node, result);
+		console.log(str);
+	}
+}
+
+try {
+	await main(process.argv.slice(2));
+} catch (e) {
+	console.error(`error: ${e instanceof Error ? e.message : e}`);
+	process.exit(1);
+}
---
+++ src/core.ts
@@ -0,0 +1,93 @@
+import * as path from "node:path";
+import ts from "typescript";
+import type { DefinitionEntry } from "./def.js";
+
+const factory = ts.factory;
+const rootIdent = factory.createIdentifier("Root");
+
+function createImportNode(def: DefinitionEntry) {
+	let importNode: ts.TypeNode = factory.createImportTypeNode(
+		factory.createLiteralTypeNode(factory.createStringLiteral(def.import)),
+		undefined,
+		factory.createIdentifier(def.type),
+		undefined,
+		false,
+	);
+
+	for (const segment of def.resolve) {
+		importNode = factory.createIndexedAccessTypeNode(
+			importNode,
+			factory.createLiteralTypeNode(factory.createStringLiteral(segment)),
+		);
+	}
+
+	return factory.createTypeAliasDeclaration(undefined, rootIdent, undefined, importNode);
+}
+
+const reserved = [
+	"break", "case", "catch", "class", "const", "continue", "debugger",
+	"default", "delete", "do", "else", "enum", "export", "extends", "false",
+	"finally", "for", "function", "if", "import", "in", "instanceof", "new",
+	"null", "return", "super", "switch", "this", "throw", "true", "try",
+	"typeof", "var", "void", "while", "with",
+];
+
+function createExportNode(checker: ts.TypeChecker, type: ts.Type) {
+	const declarations: ts.VariableDeclaration[] = [];
+	for (const prop of checker.getPropertiesOfType(type)) {
+		if (reserved.includes(prop.name)) continue;
+		declarations.push(
+			factory.createVariableDeclaration(
+				factory.createIdentifier(prop.name),
+				undefined,
+				factory.createIndexedAccessTypeNode(
+					factory.createTypeReferenceNode(rootIdent),
+					factory.createLiteralTypeNode(factory.createStringLiteral(prop.name)),
+				),
+			),
+		);
+	}
+
+	return factory.createVariableStatement(
+		[factory.createToken(ts.SyntaxKind.ExportKeyword)],
+		factory.createVariableDeclarationList(declarations, ts.NodeFlags.Const),
+	);
+}
+
+export function createModuleDeclarations(program: ts.Program, definitions: Iterable<DefinitionEntry>, resolveDir: string) {
+	const checker = program.getTypeChecker();
+	const nodes: ts.Node[] = [];
+
+	for (const def of definitions) {
+		const moduleName = factory.createStringLiteral(def.path.join("/"));
+
+		const statements: ts.Statement[] = [];
+		statements.push(createImportNode(def));
+		{
+			const source = program.getSourceFile(path.resolve(resolveDir, def.source));
+			if (!source) throw new Error("source file not found in project");
+			const symbol = checker.getSymbolAtLocation(source);
+			if (!symbol) throw new Error("source file has no symbol associated with it");
+			const exp = checker.getExportsOfModule(symbol).find((v) => v.escapedName === def.type);
+			if (!exp) throw new Error("could not find exported type");
+
+			let type = checker.getDeclaredTypeOfSymbol(exp);
+			for (const segment of def.resolve) {
+				const sym = checker.getPropertyOfType(type, segment);
+				if (!sym) throw new Error(`unknown property during traversal "${segment}"`);
+				type = checker.getTypeOfSymbol(sym);
+			}
+
+			statements.push(createExportNode(checker, type));
+		}
+
+		const moduleNode = factory.createModuleDeclaration(
+			[factory.createToken(ts.SyntaxKind.DeclareKeyword)],
+			moduleName,
+			factory.createModuleBlock(statements),
+		);
+		nodes.push(moduleNode);
+	}
+
+	return nodes;
+}
